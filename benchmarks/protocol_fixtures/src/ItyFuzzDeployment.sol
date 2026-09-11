fa7055e471bf860fc2444670dcd3dc4746b29886
tobestwitter-star <tobestwitter@gmail.com>
2026-09-11 15:26:25 +0100

    Make protocol fixture use production ItyFuzz deployment pattern

--- benchmarks/protocol_fixtures/src/ItyFuzzDeployment.sol
+++ benchmarks/protocol_fixtures/src/ItyFuzzDeployment.sol
@@ -1,13 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 import "./ProtocolFixtures.sol";
+
 contract ItyFuzzDeployment {
-    function deployAll() external {
-        new StateMachineVault();
-        new ReentrancyBank();
-        new UnreachableCandidate();
-        new InvariantPreserving();
-        new AuthorizationSequence();
-        new EconomicPool(1000, 1000);
+    StateMachineVault public stateMachine;
+    ReentrancyBank public bank;
+    UnreachableCandidate public unreachable;
+    InvariantPreserving public invariantPreserving;
+    AuthorizationSequence public authorization;
+    EconomicPool public economic;
+
+    function setUp() public {
+        stateMachine = new StateMachineVault();
+        bank = new ReentrancyBank();
+        unreachable = new UnreachableCandidate();
+        invariantPreserving = new InvariantPreserving();
+        authorization = new AuthorizationSequence();
+        economic = new EconomicPool(1000, 1000);
     }
 }
