8350b6a66896a2f5907a4f6683b641379a626ce9
tobestwitter-star <tobestwitter@gmail.com>
2026-09-11 15:49:45 +0100

    Make latest-price oracle holdout explicitly reachable

--- benchmarks/holdout/OracleLatestBug.sol
+++ benchmarks/holdout/OracleLatestBug.sol
@@ -2,5 +2,6 @@ pragma solidity ^0.8.20;
 interface ILatest { function latestPrice() external view returns(uint256,uint256); }
 contract OracleLatestBug {
  ILatest public oracle; mapping(address=>uint256) public debt;
+ constructor(ILatest a){oracle=a;}
  function borrow(uint256 collateral) external { (uint256 price,uint256 updated)=oracle.latestPrice(); require(price>0); require(collateral*price>=100 ether); debt[msg.sender]+=100 ether; }
 }
