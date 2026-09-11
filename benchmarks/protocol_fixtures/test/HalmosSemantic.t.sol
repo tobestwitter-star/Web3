aedb246d87a72094f050e79743ab14ca19f1b810
tobestwitter-star <tobestwitter@gmail.com>
2026-09-11 14:57:59 +0100

    Use Halmos-recognized semantic check fixtures

--- benchmarks/protocol_fixtures/test/HalmosSemantic.t.sol
+++ benchmarks/protocol_fixtures/test/HalmosSemantic.t.sol
@@ -2,18 +2,6 @@
 pragma solidity 0.8.20;
 import "../src/ProtocolFixtures.sol";
 contract HalmosSemanticTest {
-    function checkStateMachineSecurityProperty() public {
-        StateMachineVault v = new StateMachineVault();
-        v.prepare();
-        address other = address(0xBEEF);
-        v.execute(other, 1);
-        assert(v.credit(other) == 0);
-    }
-    function checkInvariantPreserving() public {
-        InvariantPreserving s = new InvariantPreserving();
-        s.mint(address(this), 100);
-        uint256 beforeTotal = s.total();
-        s.transfer(address(1), 40);
-        assert(s.total() == beforeTotal);
-    }
+    function check_stateMachineSecurityProperty() public { StateMachineVault v=new StateMachineVault(); v.prepare(); address other=address(0xBEEF); v.execute(other,1); assert(v.credit(other)==0); }
+    function check_invariantPreserving() public { InvariantPreserving s=new InvariantPreserving(); s.mint(address(this),100); uint256 beforeTotal=s.total(); s.transfer(address(1),40); assert(s.total()==beforeTotal); }
 }
