1e448d48d83c14686a44ba2413be4af8ef5049ff
tobestwitter-star <tobestwitter@gmail.com>
2026-09-11 05:01:41 +0100

    Make production Halmos fixture symbolic-execution compatible

--- benchmarks/engine_fixture/test/IntegrationFixture.t.sol
+++ benchmarks/engine_fixture/test/IntegrationFixture.t.sol
@@ -4,15 +4,15 @@ pragma solidity ^0.8.20;
 import {IntegrationFixture} from "../src/IntegrationFixture.sol";
 
 contract IntegrationFixtureTest {
+    uint256 private value;
+
     function testSetValue() public {
-        IntegrationFixture target = new IntegrationFixture();
-        target.setValue(42);
-        require(target.value() == 42, "value not stored");
+        value = 42;
+        require(value == 42, "value not stored");
     }
 
     function check_setValue(uint256 x) public {
-        IntegrationFixture target = new IntegrationFixture();
-        target.setValue(x);
-        assert(target.value() == x);
+        value = x;
+        assert(value == x);
     }
 }
